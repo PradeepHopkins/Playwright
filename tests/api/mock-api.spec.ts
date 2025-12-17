fa280fbf3492de3b02b09b9de1f9b6046276567e
PradeepHopkins <pradeephopkins@gmail.com>
2025-12-17 18:28:08 +0530

    routebywildcard

--- tests/api/mock-api.spec.ts
+++ tests/api/mock-api.spec.ts
@@ -2,7 +2,7 @@ import test, { expect } from "@playwright/test";
 import tags from "./test-date/tags.json"
 
 test.beforeEach('Mock API Test', async ({ page }) => {
-    page.route('https://conduit-api.bondaracademy.com/api/tags', async route => {
+    page.route('*/**/api/tags', async route => {
         await route.fulfill({
             body: JSON.stringify(tags)
         })
@@ -13,3 +13,6 @@ test.beforeEach('Mock API Test', async ({ page }) => {
 test('Mock API Test', async ({ page }) => {
     await expect(page.locator('.navbar-brand')).toHaveText('conduit')
 })
+// Instead of providing the full API URL, we can simplify the route by using wildcards (*).
+// Wildcards allow us to replace part of the base URL and match any pattern that ends with the desired path.
+// For example: **/api/tags
